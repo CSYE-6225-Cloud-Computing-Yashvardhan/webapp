a3ca54e88224ef05dbd765169dcf630890259e2f
Yashvardhan Limbodiya <limbodiya.y@northeastern.edu>
2024-09-24 14:15:02 -0400

    HealthzRoute changes

--- routes/healthzRoute.js
+++ routes/healthzRoute.js
@@ -8,10 +8,13 @@ router.use((request, response, next) => {
         return response.status(405).header('Cache-Control', 'no-cache').send();
     }
     console.log("Request Body" + request.body);
-    if (Object.keys(request.query).length > 0 || (Object.keys(request.body || {}).length > 0)) {
-        console.log("Request contains Payload");
+    console.log("Request Headers" + request.headers);
+    console.log("Request Headers contentType" + request.headers['content-type']);
+    if (Object.keys(request.query).length > 0 || (Object.keys(request.body || {}).length > 0)) { // || request.headers['content-type'] !== 'application/json' Check for contentType
+        console.log("Request contains Payload"); 
         return response.status(400).header('Cache-Control', 'no-cache').send();
     }
+
   
     next();
   });
