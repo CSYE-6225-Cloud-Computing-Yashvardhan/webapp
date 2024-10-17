77ce41821cd1e1ae6eaccdf496c54f4081274b9c
Yashvardhan Limbodiya <limbodiya.y@northeastern.edu>
2024-10-16 23:39:12 -0400

    changes made to raise pull request

--- scripts/user.sh
+++ scripts/user.sh
@@ -8,6 +8,7 @@ sudo apt upgrade -y
 echo "Creating User and user group"
 # Create the csye6225 group
 sudo groupadd csye6225
+
 # Create the csye6225 user with the primary group csye6225 and no login shell
 sudo useradd -r -g csye6225 -s /usr/sbin/nologin csye6225
 echo " - User csye6225 created."
