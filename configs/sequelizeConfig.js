dc8767d070dbedfe6c5301050895912bfc137b4c
Yashvardhan Limbodiya <limbodiya.y@northeastern.edu>
2024-11-26 01:04:04 -0500

    AWS Secrets changes added in Sequelize config

--- configs/sequelizeConfig.js
+++ configs/sequelizeConfig.js
@@ -1,16 +1,32 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
+const AWS = require('aws-sdk');
+const secretsManager = new AWS.SecretsManager();
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  dialect: 'mysql',
-  logging: false, 
-});
+let sequelize; 
 
-const database = {};
+(async () => {
+  try {
+  
+    const secret = await secretsManager.getSecretValue({
+      SecretId: process.env.DB_PASS_SECRET_ID,
+    }).promise();
+    const secretString = JSON.parse(secret.SecretString);
+    const dbPassword = secretString.password;
 
-database.Sequelize = Sequelize;
-database.sequelize = sequelize;
+    
+    sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, dbPassword, {
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      dialect: 'mysql',
+      logging: false,
+    });
 
-module.exports = database;
+    console.log('Sequelize instance initialized successfully.');
+  } catch (error) {
+    console.error('Error initializing Sequelize:', error.message, { error });
+    throw new Error('Failed to initialize Sequelize');
+  }
+})();
+
+module.exports = { sequelize, Sequelize };
