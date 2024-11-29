d1731aaa2872852ac86375d4a557cbead0dce36a
Yashvardhan Limbodiya <limbodiya.y@northeastern.edu>
2024-11-29 02:46:49 -0500

    AWS CLI installation added in scripts, DB code restore

--- configs/sequelizeConfig.js
+++ configs/sequelizeConfig.js
@@ -1,32 +1,16 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
-const AWS = require('aws-sdk');
-const secretsManager = new AWS.SecretsManager();
 
-let sequelize; 
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  dialect: 'mysql',
+  logging: false, 
+});
 
-(async () => {
-  try {
-  
-    const secret = await secretsManager.getSecretValue({
-      SecretId: process.env.DB_PASS_SECRET_ID,
-    }).promise();
-    const secretString = JSON.parse(secret.SecretString);
-    const dbPassword = secretString.password;
+const database = {};
 
-    
-    sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, dbPassword, {
-      host: process.env.DB_HOST,
-      port: process.env.DB_PORT,
-      dialect: 'mysql',
-      logging: false,
-    });
+database.Sequelize = Sequelize;
+database.sequelize = sequelize;
 
-    console.log('Sequelize instance initialized successfully.');
-  } catch (error) {
-    console.error('Error initializing Sequelize:', error.message, { error });
-    throw new Error('Failed to initialize Sequelize');
-  }
-})();
-
-module.exports = { sequelize, Sequelize };
+module.exports = database;
--- scripts/install.sh
+++ scripts/install.sh
@@ -9,6 +9,21 @@ echo "Installing Node.js and npm..."
 curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
 sudo apt install nodejs -y
 
+#Install JSON parsor
+echo "Installing JSON parsor..."
+sudo apt install -y jq
+
+
+#Install AWS CLI
+echo "Installing AWS CLI..."
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+if ! command -v aws &> /dev/null; then
+  echo "AWS CLI installation failed!"
+  exit 1
+fi
+
 # Install AWS Cloud Watch
 echo "Installing Amazon Cloud Watch..."
 wget https://s3.amazonaws.com/amazoncloudwatch-agent/ubuntu/amd64/latest/amazon-cloudwatch-agent.deb
